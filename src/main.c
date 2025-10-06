9bffd35fba7d7a9208ba3f9441453fd9df9eea5a
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-10-06 22:46:56 +0200

    handle args via getopt and add line index flag

--- src/main.c
+++ src/main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
+#include <stdbool.h>
 
 #define LINE_BUFFER 512
 
 char *pattern;
 char *filename;
+bool showLineIndex = false;
+
 
 void handleArgs(int argc, char *argv[]) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: grepr <pattern> <filename>\n");
+    int opt;
+
+    while ((opt = getopt(argc, argv, "n")) != -1) {
+        switch (opt) {
+            case 'n':
+                showLineIndex = true;
+                break;
+            default:
+                fprintf(stderr, "Usage: grepr [OPTIONS] <pattern> <filename>\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (argc - optind < 2) {
+        fprintf(stderr, "Usage: grepr [OPTIONS] <pattern> <filename>\n");
         exit(EXIT_FAILURE);
     }
 
-    pattern = argv[1];
-    filename = argv[2];
+    pattern = argv[optind];
+    filename = argv[optind + 1];
 }
 
 int main(int argc, char *argv[]) {
@@ -27,9 +44,14 @@ int main(int argc, char *argv[]) {
     }
 
     char lineBuffer[LINE_BUFFER];
+    int lineIndex = 1;
     while (fgets(lineBuffer, LINE_BUFFER, file)) {
-        if (strstr(lineBuffer, pattern))
+        if (strstr(lineBuffer, pattern)) {
+            if (showLineIndex) 
+                printf("%d: ", lineIndex);
             printf("%s", lineBuffer);
+        }
+        lineIndex++;
     }
 
     fclose(file);
