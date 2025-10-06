26bebba49d46155377499e26e0e8063aa01eda21
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-10-06 21:51:45 +0200

    implement basic functionality

--- src/main.c
+++ src/main.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char *pattern;
-char *filename;
+#define LINE_BUFFER 512
 
-void handleArgs(int argc, char *argv[]) {
+void handleArgs(int argc, char *argv[], char **pattern, char **filename) {
     if (argc < 3) {
         printf("Usage: grepr <pattern> <filename>");
         exit(1);
     }
 
-    pattern = argv[1];
-    filename = argv[2];
+    *pattern = argv[1];
+    *filename = argv[2];
 }
 
 int main(int argc, char *argv[]) {
-    handleArgs(argc, argv);
+    char *pattern;
+    char *filename;
+    handleArgs(argc, argv, &pattern, &filename);
 
-    printf("pattern: %s\nfilename: %s", pattern, filename);
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("grepr: Error opening file");
+        return 1;
+    }
+
+    char lineBuffer[LINE_BUFFER];
+    while (fgets(lineBuffer, LINE_BUFFER, file)) {
+        if (strstr(lineBuffer, pattern))
+            printf("%s", lineBuffer);
+    }
+
+    fclose(file);
 
     return 0;
 }
